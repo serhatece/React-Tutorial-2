47bb0f9b09c9a2a682a2df76b951040cdfb7ce4c
Serhat Ece <134696090+serhatece@users.noreply.github.com>
2025-01-23 11:29:12 +0300

    7-react-MUI-add

---
+++ 7-react-MUI/src/App.jsx
@@ -0,0 +1,13 @@
+import ButtonUsage from "./components/ButtonUsage";
+import TypeGraphyUsage from "./components/TypeGraphyUsage";
+
+function App() {
+  return (
+    <>
+      {/* <ButtonUsage /> */}
+      <TypeGraphyUsage />
+    </>
+  );
+}
+
+export default App;
---
+++ 7-react-MUI/src/components/TypeGraphyUsage.jsx
@@ -0,0 +1,17 @@
+import { Box, Typography } from "@mui/material";
+
+export default function TypeGraphyUsage() {
+  return (
+    <Box sx={{ width: "100%", maxWidth: 800 }}>
+      <Typography variant="h1">Lorem, ipsum dolor.</Typography>
+      <Typography variant="h2">Lorem, ipsum dolor.</Typography>
+      <Typography variant="h3">Lorem, ipsum dolor.</Typography>
+      <Typography variant="h4">Lorem, ipsum dolor.</Typography>
+      <Typography variant="h5">Lorem, ipsum dolor.</Typography>
+      <Typography variant="h6">Lorem, ipsum dolor.</Typography>
+      <Typography variant="subtitle1">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, quis!
+      </Typography>
+    </Box>
+  );
+}
