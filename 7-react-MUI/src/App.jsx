d1e008f4267c3f3879ecb2bb6d6e4f7fd00fd0e3
Serhat Ece <134696090+serhatece@users.noreply.github.com>
2025-01-26 13:29:40 +0300

    Update App.jsx

--- 7-react-MUI/src/App.jsx
+++ 7-react-MUI/src/App.jsx
@@ -5,9 +5,9 @@ import TypeGraphyUsage from "./components/TypeGraphyUsage";
 function App() {
   return (
     <>
-      <ButtonUsage />
-      <TypeGraphyUsage />
-      <TextFieldUsage />
+      {/* <ButtonUsage /> */}
+      {/* <TypeGraphyUsage /> */}
+      {/* <TextFieldUsage /> */}
     </>
   );
 }
